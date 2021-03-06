53d7098bac4f7e00974851721d6af0fca2532127
Nick <nrslvsjs@gmail.com>
2021-03-06 14:00:54 -0800

    initalize

---
+++ R/exercise.R
@@ -0,0 +1,29 @@
+#' Get an exercise
+#'
+#' @param chapter The chapter that the exercise is for
+#' @param filename What filename you want to save (defaults to the name of the chapter)
+#' @param answers Whether or not you want the answers
+#'
+#' @return Saves a file to the working directory (or path from filename)
+#' @export
+#'
+exercise <- function(chapter, filename = NULL, answers = FALSE) {
+  tag <- c("intro", "data", "ggplot", "tidyr", "dplyr", "joins", "func", "sim", "glm")
+  fname <- sprintf("book/exercises/%02d_%s_%s.Rmd",
+                      chapter, tag[chapter],
+                      ifelse(answers, "answers", "exercise"))
+  f <- system.file(fname, package = "dataskills")
+  
+  if (f == "") stop("Exercise ", chapter, " doesn't exist")
+  
+  if (is.null(filename)) {
+    filename <- gsub("^book/exercises/", "", fname)
+  }
+  
+  file.copy(f, filename)
+  
+  #open file for editing
+  if(rstudioapi::hasFun("navigateToFile")){
+    rstudioapi::navigateToFile(filename)
+  }
+}
